1127bc4fc597003678295dfe59133c7f021f8ba4
Jakub Myszka <jmyszka@jmyszka-UX31E>
2013-04-07 22:16:32 +0200

    added older configs and plugins
    added bundle for puppet and buffers
    added some config inherited end still relevant from older setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,7 +30,7 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && m
 
 if [ ! -e $endpath/.git ]; then
     echo "cloning spf13-vim"
-    git clone --recursive -b 3.0 http://github.com/spf13/spf13-vim.git $endpath
+    git clone --recursive -b 3.0 https://github.com/myszek123/spf13-vim/blob/3.0/bootstrap.sh $endpath
 else
     echo "updating spf13-vim"
     cd $endpath && git pull
