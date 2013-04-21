45e17632ed5df1e33754c873abebfd1db810aa6b
myszek123 <jakubmyszka@gmail.com>
2013-04-22 02:17:41 +0300

    Update bootstrap.sh
    quick fix for fork address

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,7 +30,7 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && m
 
 if [ ! -e $endpath/.git ]; then
     echo "cloning spf13-vim"
-    git clone --recursive -b 3.0 https://github.com/myszek123/spf13-vim/blob/3.0/bootstrap.sh $endpath
+    git clone --recursive -b 3.0 git@github.com:myszek123/spf13-vim.git $endpath
 else
     echo "updating spf13-vim"
     cd $endpath && git pull
